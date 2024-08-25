10519214cbf384035b3812a1eae2701106ffaa2f
kayongopique <kayongopique@gmail.com>
2024-08-25 22:05:42 +0800

    feat(blinky) Blink test led using custom port and pin config
    this is to test whether our blink app still runs when we substittute
    with custom configurations

--- src/main.c
+++ src/main.c
@@ -1,14 +1,29 @@
 #include<msp430.h>
 #include "app/led.h"
+#include<drivers/io.h>
+
+static void test_blink_led(void) {
+    const struct  io_config led_config = 
+    {
+        /* data */
+        .select = IO_SELECT_GPIO,
+        .dir = IO_DIR_OUTPUT,
+        .resistor = IO_RESISTOR_DISABLED,
+        .out = IO_OUT_LOW
+
+    };
+    io_configure(IO_TEST_LED, &led_config);
+}
 
 int main(void) {
     volatile unsigned int i;
     WDTCTL = WDTPW + WDTHOLD; // stop watchdog timer
-    led_init();
+    io_out_e out = IO_OUT_LOW;
 
     while (1)
     {
-        led_toggle();
+        out = (out == IO_OUT_LOW)? IO_OUT_HIGH: IO_OUT_LOW;
+        io_set_out(IO_TEST_LED, out);
         for ( i = 10000; i >0; i--);   // delay
         
     }
